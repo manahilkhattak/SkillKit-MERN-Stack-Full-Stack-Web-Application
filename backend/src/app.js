d5b1b537527a41e1e9728407ecc29fd149d103c3
Manahil Khattak <manahilkhattak46@gmail.com>
2026-05-08 21:32:13 +0500

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const routes = require('./routes/index');
 const app = express();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || ['http://localhost:5173','https://skill-kit-mern-stack-full-stack-web-application-cnldfw6lq.vercel.app'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
